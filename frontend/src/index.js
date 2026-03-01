11e5ee9872b67bd29c0b801b5106aa5e82bc2e69
Nidish2 <22cse059@bnmit.in>
2026-03-01 23:38:16 +0530

    Deployment changes for CI-CD Pipeline

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,10 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import ChatProvider from "./Context/ChatProvider";
+import axios from "axios";
 
+axios.defaults.baseURL =
+  "https://chit-chat-a-real-time-chatting-app.onrender.com";
 // Create the root element for React rendering
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
